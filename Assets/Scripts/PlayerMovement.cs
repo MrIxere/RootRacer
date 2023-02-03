404b4ec1cb5b952326bc8329985ba515fe52b5ed
Florian Rossignol <rossignol.florian.fr@gmail.com>
2023-02-03 21:19:56 +0100

    add script for walk and assets push

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,20 +1,27 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    [SerializeField] private float dx;
-    [SerializeField] private float dy;
+    private Vector2 Movement;
+    [SerializeField] private Rigidbody2D Playerbody;
+    private const float Speed = 2.0f;
     // Start is called before the first frame update
     void Start()
     {
-        
+        Playerbody = GetComponent<Rigidbody2D>();
+    }
+    void Update()
+    {
+        Movement.x = Input.GetAxis("Horizontal");
+        Movement.y = Input.GetAxis("Vertical");
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        
+       Playerbody.MovePosition(Playerbody.position + Movement * Speed);
     }
 }
