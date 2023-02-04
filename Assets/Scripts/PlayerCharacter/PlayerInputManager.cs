3753a214757abd35d69ef4eb93e47b269ee593fe
Florian Rossignol <rossignol.florian.fr@gmail.com>
2023-02-04 16:50:25 +0100

    push player input manager
    push

--- Assets/Scripts/PlayerCharacter/PlayerInputManager.cs
+++ Assets/Scripts/PlayerCharacter/PlayerInputManager.cs
@@ -1,18 +1,33 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerInputManager : MonoBehaviour
 {
+    [SerializeField] private Array _player_array;
     // Start is called before the first frame update
-    void Start()
+    private void Awake()
     {
-        
+        if (_player_array == _player_array.GetValue(0))
+        {
+            
+        }
+        else
+        {
+            
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    public void OnMove(InputValue value)
+    {
+        value.Get<Vector2>();
     }
 }
