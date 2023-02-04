1cfe5ea54ff6a869238d59c1e4dc624ebfbc1e19
Paul <paul.breuza@gmail.com>
2023-02-04 23:45:11 +0100

    arg

--- Assets/Scripts/ScreenShake.cs
+++ Assets/Scripts/ScreenShake.cs
@@ -6,8 +6,7 @@ using UnityEngine.InputSystem;
 public class ScreenShake : MonoBehaviour
 {
 
-    [SerializeField] private AnimationCurve curve;
-    [SerializeField] private float duration = 3.0f;
+    
     
     // Start is called before the first frame update
     void Start()
@@ -21,25 +20,10 @@ public class ScreenShake : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.Space))
         { 
             Debug.Log("Shake");
-            StartCoroutine(Shake());
+           
         }
     }
 
-    IEnumerator Shake()
-    {
-        
-        Vector3 startPosition = transform.position;
-        float elapsedTime = 0.0f;
-        while (elapsedTime < duration)
-        {
-            elapsedTime += Time.deltaTime;
-            float strength = curve.Evaluate(elapsedTime / duration);
-            transform.position = startPosition + Random.insideUnitSphere * strength;
-            yield return null;
-        }
-
-        //transform.position = startPosition;
-
-    }
+   
     
 }
