1607632757fe960c5dd4d5f05448683cbd4a52b1
Florian Rossignol <rossignol.florian.fr@gmail.com>
2023-02-04 15:59:53 +0100

    add some movement
    work on movement

--- Assets/Scripts/PlayerScript.cs
+++ Assets/Scripts/PlayerScript.cs
@@ -15,6 +15,9 @@ public class PlayerScript : MonoBehaviour
     private _Player _currentPlayer;
     private float _Attenuation = 10.0f / 5.0f;
 
+    private Vector2 _addforce;
+        //InputActionMap _ip;
+
     enum _Player
     {
         PLAYERONE,
@@ -30,27 +33,36 @@ public class PlayerScript : MonoBehaviour
             _currentPlayer = _Player.PLAYERTWO;
         }
         _currentPlayer = _Player.PLAYERONE;
+        _addforce.x = 0.0f;
+        _addforce.y = -1.0f;
     }
 
     private void FixedUpdate()
     {
+        
         _Player_Body.AddForce(_movement * _speed / _Attenuation);
-        
+        _Player_Body.velocity = _addforce;
+       
         //TODO add moove split in two direction
-        
     }
 
     public void OnMove(InputValue value)
     {
         _movement = value.Get<Vector2>();
+        for (int i = 0; i < 2; i++)
+        {
+            var _interpolation = _Player_Body.interpolation;
+            _Player_Body.interpolation = _interpolation;
+        }
         switch (_currentPlayer)
         {
             case _Player.PLAYERONE:
                 //InputSystem.FindControl("Move");
+                //_ip.FindAction("WASD");
                 break;
             case _Player.PLAYERTWO:
                 //InputSystem.FindControls("Move");
-                
+                //_ip.FindAction("MoveTwo");
                 break;
             default:
                 throw new ArgumentOutOfRangeException();
