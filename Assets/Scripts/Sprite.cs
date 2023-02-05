e55d93951071fbd7ac391d3b46cf6ae61b63ea75
Paul <paul.breuza@gmail.com>
2023-02-05 12:53:22 +0100

    arg

--- Assets/Scripts/Sprite.cs
+++ Assets/Scripts/Sprite.cs
@@ -11,31 +11,33 @@ public class Sprite : MonoBehaviour
     [SerializeField] UnityEngine.Sprite sprite3;
     [SerializeField] UnityEngine.Sprite sprite4;
     [SerializeField] UnityEngine.Sprite sprite5;
-    private SpriteRenderer renderer;
+    //private SpriteRenderer renderer;
     private int rand;
 
     private void Start()
     {
+        SpriteRenderer spriteRenderer = GetComponent<SpriteRenderer>();
         rand = Random.Range(0, 6);
+        Debug.Log(rand);
         if (rand == 0)
         {
-            renderer.sprite = sprite1;
+             spriteRenderer.sprite = sprite1;
         }
         if (rand == 1)
         {
-            renderer.sprite = sprite2;
+             spriteRenderer.sprite = sprite2;
         }
         if (rand == 2)
         {
-            renderer.sprite = sprite3;
+             spriteRenderer.sprite = sprite3;
         }
         if (rand == 3)
         {
-            renderer.sprite = sprite4;
+             spriteRenderer.sprite = sprite4;
         }
         if (rand == 4)
         {
-            renderer.sprite = sprite5;
+             spriteRenderer.sprite = sprite5;
         }
     }
 }
