f0cf87352ae7664e1dad91db286321a2a0ed4ccb
Paul <paul.breuza@gmail.com>
2023-02-05 10:41:16 +0100

    arg

--- Assets/Scripts/LevelConstructor.cs
+++ Assets/Scripts/LevelConstructor.cs
@@ -6,37 +6,102 @@ public class LevelConstructor : MonoBehaviour
 {
     // Start is called before the first frame update
 
-    [SerializeField] private GameObject prefab1;
-    [SerializeField] private GameObject prefab2;
-    [SerializeField] private GameObject prefab3;
+    [SerializeField] private GameObject prefab1_1;
+    [SerializeField] private GameObject prefab1_2;
+    [SerializeField] private GameObject prefab2_1;
+    [SerializeField] private GameObject prefab2_2;
+
+    [SerializeField] private GameObject prefab3_1;
+    [SerializeField] private GameObject prefab3_2;
+    [SerializeField] private GameObject prefab4_1;
+    [SerializeField] private GameObject prefab4_2;
+
+    private int firstPool = -1;
+    private int secondPool = -1;
+    private int thirdPool = -1;
+    private int fourthPool = -1;
+    
 
     void Start()
     {
         float BlocY = 0.0f;
-        for (int i = 0; i < 6; i++)
+        for (int i = 0; i < 4; i++)
         {
-            int rand = Random.Range(0, 3);
+            int rand = Random.Range(0, 8);
+            while (rand == firstPool)
+            {
+                rand = Random.Range(0, 8);
+            }
+            while (rand == secondPool)
+            {
+                rand = Random.Range(0, 8);
+            }
+            while (rand == thirdPool)
+            {
+                rand = Random.Range(0, 8);    
+            }
+            while (rand == fourthPool)
+            {
+                rand = Random.Range(0, 8);
+            }
+            
+            
+
             if (rand == 0)
             {
-                Instantiate(prefab1, new Vector3(0, (-38.21f + BlocY), 0), Quaternion.identity);
+                Instantiate(prefab1_1, new Vector3(0, (-38.21f + BlocY), 0), Quaternion.identity);
             }
             if (rand == 1)
             {
-                Instantiate(prefab2, new Vector3(0, (-38.21f + BlocY), 0), Quaternion.identity);
+                Instantiate(prefab1_2, new Vector3(0, (-38.21f + BlocY), 0), Quaternion.identity);
             }
             if (rand == 2)
             {
-                Instantiate(prefab3, new Vector3(0, (-38.21f + BlocY), 0), Quaternion.identity);
+                Instantiate(prefab2_1, new Vector3(0, (-38.21f + BlocY), 0), Quaternion.identity);
             }
+            if (rand == 3)
+            {
+                Instantiate(prefab2_2, new Vector3(0, (-38.21f + BlocY), 0), Quaternion.identity);
+            }
+            if (rand == 4)
+            {
+                Instantiate(prefab3_1, new Vector3(0, (-38.21f + BlocY), 0), Quaternion.identity);
+            }
+            if (rand == 5)
+            {
+                Instantiate(prefab3_2, new Vector3(0, (-38.21f + BlocY), 0), Quaternion.identity);
+            }
+            if (rand == 6)
+            {
+                Instantiate(prefab4_1, new Vector3(0, (-38.21f + BlocY), 0), Quaternion.identity);
+            }
+            if (rand == 7)
+            {
+                Instantiate(prefab4_2, new Vector3(0, (-38.21f + BlocY), 0), Quaternion.identity);
+            }
+            
+            
+            if (i == 0)
+            {
+                firstPool = rand;
+            }
+            if (i == 1)
+            {
+                secondPool = rand;
+            }
+            if (i == 2)
+            {
+                thirdPool = rand;
+            }
+            if (i == 3)
+            {
+                fourthPool = rand;
+            }
+            
+            
             BlocY -= 85.79f;
             Debug.Log(rand);
         }
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-
     }
 }
 
